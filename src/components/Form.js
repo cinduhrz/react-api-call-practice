7c7e60d2404f11c739b1387175469d6db83068b0
Cindy Wong <cindywongdev@gmail.com>
2022-12-21 19:12:54 -0500

    created components folder, created shell for form and moviedisplay components

---
+++ src/components/Form.js
@@ -0,0 +1,7 @@
+function Form(props){
+    return (
+        <h1>Form</h1>
+    )
+}
+
+export default Form
---
+++ src/components/MovieDisplay.js
@@ -0,0 +1,7 @@
+function MovieDisplay(props){
+    return (
+        <h1>Movie Display</h1>
+    )
+}
+
+export default MovieDisplay
